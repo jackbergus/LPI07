504e5f574b06c9dd5f45865d8b9d4a43d50b401c
Giacomo Bergami <bergamigiacomo@gmail.com>
2017-09-08 23:27:04 +0200

    done

---
+++ Lesson00/MinimalWorking.java
@@ -0,0 +1,8 @@
+/* This is a minimal working program
+ * The interpreter finds the class, that matches with the file name, and the program's entry point
+ * On the other hand, this program does nothing. How can we output a string?
+ */
+class MinimalWorking {
+	public static void main(String[] args) {
+	}
+}
