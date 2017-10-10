15d116e075e12502de547b760e4fca38f22bb81e
Giacomo Bergami <bergamigiacomo@gmail.com>
2017-10-10 13:47:27 +0200

    Check-o

---
+++ Lesson00/creation.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+mkdir Tom
+mkdir -p Tom/Data
+touch Tom/Data/One.txt
+touch Tom/Data/Two.txt
+mkdir -p Tom/Thesis
+touch Tom/Notes.txt
+mkdir -p Tom/Tools/Old
+touch Tom/Tools/Format
+touch Tom/Tools/Stats
