5a6400779190614886cf9216a7fe9157d853db85
Giacomo Bergami <bergamigiacomo@gmail.com>
2017-10-23 21:17:50 +0200

    New version

--- Lesson01/TimeConverter.java
+++ Lesson01/TimeConverter.java
@@ -33,7 +33,7 @@ class TimeConverter {
         input = input.replace("m","");
         boolean isSeconds = input.contains("s");
         input = input.replace("s","");
-	double conv1 = (isYear ? 1 : (isDay ? 365 : (isHour ? 365*24 : (isMinute ? 365*24*60 : 365*24*60*60))));
+	double conv1 = (isYear ? 365*24*60*60 : (isDay ? 24*60*60 : (isHour ? 60*60 : (isMinute ? 60 : 1))));
 
         boolean toYear = input.contains("Y");
         input = input.replace("Y","");
@@ -46,45 +46,13 @@ class TimeConverter {
         boolean toSeconds = input.contains("S");
         input = input.replace("S","");
         input = input.trim();
-	double conv2 = toYear ? 1 : (toDay ? 365 : (toHour ? 365*24 : (toMinute ? 365*24*60 : 365*24*60*60)));
+	double conv2 = toYear ? 365*24*60*60 : (toDay ? 24*60*60 : (toHour ? 60*60 : (toMinute ? 60 : 1)));
 
         int unit1 = isYear ? 4 : (isDay ? 3 : (isHour ? 2 : (isMinute ? 1 : 0)));
         int unit2 = toYear ? 4 : (toDay ? 3 : (toHour ? 2 : (toMinute ? 1 : 0)));
         double value = Double.valueOf(input);
 
-        double toreturn = value * conv2 / conv1;
-
-	/* Advanced
-
-	// Checks if the conversion implies a multiplication (true) or a division (false)
-        boolean grow = unit1 > unit2;
-        boolean originalGrow = grow;
-
-	// I adjust the "indices" for returning the right conversion for variable conversion
-	//unit1 = grow ? unit1-1 : unit1;
-	//unit2 = grow ? unit2-1 : unit2; 
-	//unit2 = unit2 < 0 ? 0 : unit2;
-        if (grow) {
-            unit1--;
-            unit2--;
-            unit2 = unit2 < 0 ? 0 : unit2;
-        }
-       
-	// See the first lesson, where the iteration was defined by a boolean.
-        // this means iterate the body until the condition stated in the body is met
-        while (unit1 != unit2) {
-            // Gets the integer to be returned for the conversion
-            double conversion = (unit1 == 0 ? 60 : (unit1 == 1 ? 60 : (unit1 == 2 ?  24 : 365)));
-
-            // Depending if the conversion requires to multiply or divide
-            // What does it happens if I replace 1.0 with 1 ??
-            toreturn *= (grow ? conversion : (1.0/conversion));
-
-            // Depending if the conversion is decreasing or increasing the unit of measurement,
-            // I want to reach the final unit of measurement
-            unit1 += (grow ? (-1) : (+1));
-        }
-	*/
+        double toreturn = value * conv1 / conv2;
 
         System.out.println(toreturn);
 
